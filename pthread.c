e9903fe43acf428ba5eebcd1b5a4408a1194a3be
Tim Costa <costat@lafayette.edu>
2014-05-04 14:08:09 -0400

    stuff

--- pthread.c
+++ pthread.c
@@ -187,64 +187,18 @@ struct Schedular * makeSchedular(TCB * main_block) {
 // Initialize the mutex
 int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr) {
 
-	mutex->lock = 0;
-	mutex->sig = _PTHREAD_MUTEX_SIG;
-	if (attr) {
-		if(attr->sig != _PTHREAD_MUTEX_ATTR_SIG) {
-			return EINVAL;
-		}
-		mutex->prioceiling = attr->prioceiling;
-		mutex->protocol = attr->protocol;
-	}else{
-		mutex->prioceiling = _PTHREAD_DEFAULT_PRIOCEILING;
-		mutex->protocol = _PTHREAD_DEFAULT_PROTOCOL;
-	}
-	mutex->owner = (pthread_t)NULL;
-	mutex->next = (pthread_mutex_t *)NULL;
-	mutex->prev = (pthread_mutex_t *)NULL;
-	mutex->busy = (pthread_cond_t *)NULL;
-	mutex->waiters = 0;
-	mutex->cond_lock = 0;
-	mutex->sem = MACH_LOCK_NULL;
-	return ESUCCESS;
 }
 
 // Destroy the mutex
 int pthread_mutex_destroy(pthread_mutex_t *mutex) {
 	
-	if(mutex->sig != _PTHREAD_MUTEX_SIG) return EINVAL;
-	if(mutex->owner != (pthread_t)NULL || mutex->busy != (pthread_cond_t *)NULL) return EBUSY;
-	mutex->sig = _PTHREAD_NO_SIG;
-	return ESUCCESS;
 
 }
 
 // Lock the mutex
 int pthread_mutex_lock(pthread_mutex_t *mutex) {
 
-	kern_return_t res;
-	if(mutex->sig == _PTHREAD_MUTEX_SIG_init) {
-		int resp;
-		if(resp=pthread_mutex_init(mutex,NULL)) return resp;
-	}
-	if(mutex->sig != _PTHREAD_MUTEX_SIG) return EINVAL;
-	mutex->lock = 1;
-	if(mutex->waiters || mutex->owner != (pthread_t)NULL) {
-		mutex->waiters++;
-	if(mutex->sem == MACH_PORT_NULL) mutex->sem = new_sem_from_pool();
-	mutex->lock = 0;
-	do {
-		PTHREAD_MACH_CALL(semaphore_wait(mutex->sem),res);
-	}while(res==KERN_ABORTED);
-	mutex->lock = 1;
-	mutex->waiters--;
-	if(mutex->waiters==0){
-		return_sem_to_pool(mutex->sem);
-		mutex->sem = MACH_PORT_NULL;
-	}
-	if(mutex->cond_lock) mutex->cond_lock = 0;
-	mutex->lock = 0;
-	return ESUCCESS;
+
 
 }
 
@@ -252,8 +206,8 @@ int pthread_mutex_lock(pthread_mutex_t *mutex) {
 int pthread_mutex_unlock(pthread_mutex_t *mutex) {
 
 	// Fill in
-
-	return 0;
+	mutex->lock = 0;
+	return 1;
 }
 
 
