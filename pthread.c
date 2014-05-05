ffcd57e31e231504f6e99c5530ff94f9f42653b7
Tim Costa <tim@chalkupedu.com>
2014-05-04 22:01:03 -0400

    r-w

--- pthread.c
+++ pthread.c
@@ -264,6 +264,7 @@ int pthread_mutex_lock(pthread_mutex_t *mutex) {
 int pthread_mutex_unlock(pthread_mutex_t *mutex) {
 
 	schedular->action = 6;
+	printf("x: %d",mutex->__data.__count);
 	schedular->currMutexVarId = mutex->__data.__count;
 	swapcontext(&schedular->head->thread_cb->thread_context, &schedular->sched_context);
 	mutex->__data.__lock = 0;
