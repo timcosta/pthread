03854f873d5d61afbf27edf1ef4a38bccbc9e6c6
Tim Costa <tim@chalkupedu.com>
2014-05-05 00:02:10 -0400

    r-w

--- test.c
+++ test.c
@@ -63,12 +63,12 @@ pthread_mutex_t pcm;
 void * producer() {
 	int count = 0;
 	do {
-		printf("producing: %d\n",shared);
+		//printf("producing: %d\n",shared);
 		count++;
 		pthread_mutex_lock(&pcm);
 		shared += 2;
 		pthread_yield();
-		printf("afteryield\n");
+		//printf("afteryield\n");
 		pthread_mutex_unlock(&pcm);
 	}while(count<10);
 }
@@ -76,7 +76,7 @@ void * producer() {
 void * consumer() {
 	int count = 0;
 	do {
-		printf("consuming: %d\n",shared);
+		//printf("consuming: %d\n",shared);
 		count++;
 		pthread_mutex_lock(&pcm);
 		shared--;
@@ -102,8 +102,8 @@ void main(void) {
 	printf("Above, you should have seen Starting followed by First, Second, Third, and Fourth printed out in order.\n");
 	printf("The expected values are 1 and 2, the same as their thread ids.\n");
 
-	printf("\n\n\nThe Readers-Writers Problem\n");
-	printf("This problem uses both a mutex and a conditional variable.\n");
+
+	printf("\n\n\nProducer-Consumer Problem\n");
 
 	pthread_mutex_init(&pcm);
 
@@ -113,20 +113,27 @@ void main(void) {
 	pthread_join(pct1,NULL);
 	pthread_join(pct2,NULL);
 
-	// pthread_cond_init(&wrt,NULL);
-	// pthread_mutex_init(&mutex,NULL);
+	printf("%d at the end. 10 expected.",shared);
 
-	// pthread_create(&w1, NULL, &writer, NULL);
-	// pthread_create(&r1, NULL, &reader, NULL);
-	// //pthread_create(&r2, NULL, &reader, NULL);
-	// //pthread_create(&r3, NULL, &reader, NULL);
-	// //pthread_create(&r4, NULL, &reader, NULL);
+	printf("\n\n\nThe Readers-Writers Problem\n");
+	printf("This problem uses both a mutex and a conditional variable.\n");
 
-	// pthread_join(r1,NULL);
-	// pthread_join(w1,NULL);
-	//pthread_join(r2,NULL);
-	//pthread_join(r3,NULL);
-	//pthread_join(r4,NULL);
+
+
+	pthread_cond_init(&wrt,NULL);
+	pthread_mutex_init(&mutex,NULL);
+
+	pthread_create(&w1, NULL, &writer, NULL);
+	pthread_create(&r1, NULL, &reader, NULL);
+	//pthread_create(&r2, NULL, &reader, NULL);
+	//pthread_create(&r3, NULL, &reader, NULL);
+	//pthread_create(&r4, NULL, &reader, NULL);
+
+	pthread_join(r1,NULL);
+	pthread_join(w1,NULL);
+	pthread_join(r2,NULL);
+	pthread_join(r3,NULL);
+	pthread_join(r4,NULL);
 
 	printf("Readers-Writers problem completed.\n");
 	printf("End of test sequence.\n");
