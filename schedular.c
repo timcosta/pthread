5cc5b7c4b79aa67b419b65685b4bee835c8532b3
Tim Costa <tim@chalkupedu.com>
2014-05-04 22:18:11 -0400

    r-w

--- schedular.c
+++ schedular.c
@@ -291,6 +291,7 @@ void waitOnCond (Schedular *s) {
 	// Get the first node in the queue 
 	Node * temp = condVarMap[s->currCondVarId];
 	printf("wc1\n");
+	printf("%d\n",temp->thread_cb->thread_id);
 	// Add the current thread to the back of the list
 	if (temp == NULL) {
 		printf("wc1.5: %d\n",s->currCondVarId);
