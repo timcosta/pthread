160385718592ee2b9216c9585a8bef049ac615bb
Tim Costa <tim@chalkupedu.com>
2014-05-04 21:04:18 -0400

    stuff

--- test.c
+++ test.c
@@ -6,13 +6,31 @@ pthread_cond_t wrt;
 int readCount = 0;
 
 void reader() {
-	printf("reading");
+	count = 0;
+	do {
+		pthread_mutex_lock(&mutex);
+		readCount++;
+		if(readCount==1) pthread_cond_wait(&wrt);
+		pthread_mutex_unlock(&mutex);
+		printf("reading...");
+		pthread_mutex_lock(&mutex);
+		readCount--;
+		if(readCount==0) pthread_cond_signal(&wrt);
+		pthread_mutex_unlock(&mutex);
+		count++;
+	} while(count<10);
 }
 
 void writer() {
-
-	printf("writing");
-
+	int count = 0;
+	do {
+		pthread_cond_wait(&wrt);
+		printf("writing...");
+		sleep(5)
+		printf("done writing.");
+		pthread_cond_signal(&wrt);
+		count++;
+	} while(count < 5);
 }
 
 void * first_message() {
@@ -33,10 +51,9 @@ void * second_message() {
 
 void main(void) {
 
-	pthread_t t1;
-	pthread_t t2;
+	pthread_t t1,t2,w1,r1,r2,r3,r4;
 
-
+	printf("Proof of concept:\n");
 	pthread_create(&t1, NULL, &first_message, NULL);
 	pthread_create(&t2, NULL, &second_message, NULL);
 	printf("Starting...\n");
@@ -46,7 +63,28 @@ void main(void) {
 	int* val2;
 	pthread_join(t2,(void**)&(val2));
 	printf("val from 2: %d\n",*val2);
-	printf("last action\n");
-	exit(0);
+	printf("Above, you should have seen First, Second, Third, and Fourth printed out in order.\n");
+	printf("The expected values are 1 and 2, the same as their thread ids.\n");
+
+	printf("\n\n\nThe Readers-Writers problem\n");
+	printf("This problem uses both a mutex and a conditional variable.\n");
+
+	pthread_create(&w1, NULL, &writer, NULL);
+	pthread_create(&r1, NULL, &reader, NULL);
+	pthread_create(&r2, NULL, &reader, NULL);
+	pthread_create(&r3, NULL, &reader, NULL);
+	pthread_create(&r4, NULL, &reader, NULL);
+
+	printf("Initialized 1 Writer and 4 readers\n");
+
+	pthread_join(r1,NULL);
+	pthread_join(w1,NULL);
+	pthread_join(r2,NULL);
+	pthread_join(r3,NULL);
+	pthread_join(r4,NULL);
+
+	printf("Readers-Writers problem completed.\n");
+	printf("End of test sequence.\n");
+
 }
 
