3ef62b1420d672e2dc6ddcbd772c2b5056f32b29
Tim Costa <tim@chalkupedu.com>
2014-05-04 23:25:55 -0400

    r-w

--- schedular.c
+++ schedular.c
@@ -453,9 +453,9 @@ void printReadyQueue(Schedular *s) {
 
 	Node * temp = s->head;
 	printf("Printing Queue:\n");
-	while (temp!=NULL) {
+	if (temp!=NULL) {
 		printf("%d",temp->thread_cb->thread_id);
-		temp = temp->next;
+		//temp = temp->next;
 	}
 	printf("NULL\n");
 
