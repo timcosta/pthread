5a53b363a58f8afa561d5baf2281fd69b00fbaa6
Tim Costa <tim@chalkupedu.com>
2014-05-04 20:01:00 -0400

    stuff

--- pthread.c
+++ pthread.c
@@ -96,12 +96,12 @@ int pthread_join(pthread_t thread, void **value_ptr) {
 	// swap to schedular context to perform join
 	swapcontext(&schedular->head->thread_cb->thread_context, &schedular->sched_context);
 
-	//printf("j3\n");
+	printf("j3\n");
 
 	// Set the join val
-	*value_ptr = (int*)schedular->head->thread_cb->join_val;
+	**value_ptr = (int*)schedular->head->thread_cb->join_val;
 	
-	//printf("j4\n");
+	printf("j4\n");
 
 	return 0;
 }
